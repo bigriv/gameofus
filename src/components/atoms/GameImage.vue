80b9b7a8be80ce26f31083b90e0adf1037d40126
yoshiwo13 <pandemiccer@gmail.com>
2022-11-20 18:41:30 +0900

    add: 基本コンポーネント作成 - 画像挿入コンポーネント

---
+++ src/components/atoms/GameImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="image-wrap"><img :src="image" /></div>
+</template>
+
+<script>
+import { defineComponent, computed } from "vue";
+import { IMAGE_ROOT_PATH } from "/src/composables/utils/const";
+
+export default defineComponent({
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const image = computed(() => IMAGE_ROOT_PATH + props.src);
+    return {
+      image,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.image-wrap {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
