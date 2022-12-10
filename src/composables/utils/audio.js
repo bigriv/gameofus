b0ab0bab9a75c825836b38ffe9146cf42a1a9465
yoshiwo13 <pandemiccer@gmail.com>
2022-12-10 23:37:02 +0900

    add: オーディオファイル追加

---
+++ src/composables/utils/audio.js
@@ -0,0 +1,16 @@
+const AUDIO = {
+  ATTACK: "attack.mp3",
+  BADEND: "badend.mp3",
+  BUTTON: "button.mp3",
+  DISPAIR: "dispair.mp3",
+  EXCLAMATION: "exclamation.mp3",
+  FANFARE: "fanfare.mp3",
+  GA: "ga.mp3",
+  HAPPYEND: "happyend.mp3",
+  MONEY: "money.mp3",
+  POWERUP: "powerup.mp3",
+  SHOUT: "shout.mp3",
+  SLEEP: "sleep.mp3",
+};
+
+export default AUDIO;
--- src/composables/utils/const.js
+++ src/composables/utils/const.js
@@ -1,1 +1,2 @@
 export const IMAGE_ROOT_PATH = "/src/assets/images/";
+export const AUDIO_ROOT_PATH = "/src/assets/audios/";
